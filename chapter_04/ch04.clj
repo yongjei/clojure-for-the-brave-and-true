c243ce51aeddd0ab0f7618d71c56f32e4ede4a6a
Yongjei Kang <yongjei.kang@gmail.com>
2026-01-28 09:05:14 +0900

    4장 연습 추가

---
+++ chapter_04/ch04.clj
@@ -0,0 +1,94 @@
+(defn titleize
+  [topic]
+  (str topic " for the Brave and True"))
+
+(map titleize ["Hamsters" "Ragnarok"])
+(map titleize '("Empathy" "Decorating"))
+(map titleize #{"Elbows" "Soap Carving"})
+(map #(titleize (second %)) {:uncomfortable-thing "Winking"})
+
+(seq '(1 2 3))
+(seq [1 2 3])
+(seq #{1 2 3})
+(seq {:name "Bill Compton" :occupation "Dead mopey guy"})
+
+(into {} (seq {:a 1 :b 2 :c 3}))
+
+(def human-consumption [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
+(map unify-diet-data human-consumption critter-consumption)
+
+(def sum #(reduce + %))
+(def avg #(/ (sum %) (count %)))
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+(stats [3 4 10])
+(stats [80 1 44 13 6])
+
+(def identities
+  [{:alias "Batman" :real "Bruce Wayne"}
+   {:alias "Spider-Man" :real "Peter Parker"}
+   {:alias "Santa" :real "Your mom"}
+   {:alias "Easter Bunny" :real "Your dad"}])
+(map :real identities)
+
+(reduce (fn [new-map [key val]]
+          (assoc new-map key (inc val)))
+        {}
+        {:max 30 :min 10})
+
+(reduce (fn [new-map [key val]]
+          (if (> val 4)
+            (assoc new-map key val)
+            new-map))
+        {}
+        {:human 4.1
+         :critter 3.9})
+
+(take 3 [1 2 3 4 5 6 7 8 9 10])
+(drop 3 [1 2 3 4 5 6 7 8 9 10])
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+(filter #(< (:human %) 5) food-journal)
+(some #(> (:critter %) 5) food-journal)
+(some #(> (:critter %) 3) food-journal)
+
+(sort [3 1 2])
+(sort-by count ["aaa" "c" "bb"])
+
+(concat [1 2] [3 4])
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true :name "McMackson"}
+   2 {:makes-blood-puns? true, :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true, :has-pulse? true :name "Mickey Mouse"}})
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+(time (vampire-related-details 0))
+(time (def mapped-details (map vampire-related-details (range 0 1000000))))
+(time (def mapped-details (doall (map vampire-related-details (range 0 1000000)))))
