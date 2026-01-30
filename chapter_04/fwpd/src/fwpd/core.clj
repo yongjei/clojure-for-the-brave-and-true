66665da9543220dc0ea7c4a22f13c7651b7bb1ad
Yongjei Kang <yongjei.kang@gmail.com>
2026-01-30 13:49:44 +0900

    4장 예제 완료

--- chapter_04/fwpd/src/fwpd/core.clj
+++ chapter_04/fwpd/src/fwpd/core.clj
@@ -8,6 +8,7 @@
   "Reads and prints the suspects CSV file."
   [& args]
   (println content))
+
 (slurp filename)
 
 (def vamp-keys [:name :glitter-index])
@@ -24,3 +25,29 @@
   ((get conversions vamp-key) value))
 
 (convert :glitter-index "3")
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
+(parse (slurp filename))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(first (mapify (parse (slurp filename))))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(glitter-filter 3 (mapify (parse (slurp filename))))
